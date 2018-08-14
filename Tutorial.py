1ece8f69da236e2b0c5d5b8daacd61c0f88e3bca
ChebaCheba <chebacheba0307@gmail.com>
2018-08-14 09:40:39 -0500

    Update Tutorial.py

--- Tutorial.py
+++ Tutorial.py
@@ -4,3 +4,9 @@ All you have to do is type return "hello edabit.com" on the second line and clic
 
 def hello():
 	return "hello edabit.com"
+
+'Create a function that takes a list and a string as arguments and return the index of the string.'
+
+def find_index(lst, str):
+	return lst.index(str)
+
